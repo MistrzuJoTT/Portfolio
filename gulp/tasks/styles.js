9879713aa9e60b5e17fb340c85bdaec2f517df78
MistrzuJoTT <jamana113@gmail.com>
2017-11-07 16:21:43 +0100

    finished basics of website

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+     autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function () {
+  return gulp.src('./app/assets/styles/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
